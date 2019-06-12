0377753bf4e6d4e40d639ab7b3ebb242a5dad01f
unknown <dungle160396@gmail.com>
2019-06-12 11:46:55 +0700

    [profile] add test result list

--- resources/views/public/user/result_list.blade.php
+++ resources/views/public/user/result_list.blade.php
@@ -1,1 +1,36 @@
-@dd($results);
+@extends('public.module.master')
+@section('content')
+  <div id="wrapper">
+    <div></div>
+		<div class="box">
+			<div class="box-header with-border">
+				<h3 class="box-title">@lang('result.list')</h3>
+			</div>
+			<div class="box-body">
+				<table class="table table-bordered">
+					<tr>
+						<th style="width: 10px">@lang('common.table.num')</th>
+						<th>@lang('result.table.test')</th>
+						<th>@lang('result.table.listening_score')</th>
+						<th>@lang('result.table.reading_score')</th>
+						<th>@lang('result.table.total_score')</th>
+						<th>@lang('result.table.test_date')</th>
+					</tr>
+					@foreach ($results as $key => $result)
+						<tr>
+							<td>{{ $key + 1 }}</td>
+							<td>{{ $result->test->name }}</td>
+							<td>{{ $result->listening_score }}</td>
+							<td>{{ $result->reading_score }}</td>
+							<td>{{ $result->listening_score + $result->reading_score }}</td>
+							<td>{{ date('d-m-Y', strtotime($result->created_at)) }}</td>
+						</tr>
+					@endforeach
+				</table>
+			</div>
+			<div class="box-footer clearfix">
+			</div>
+		</div>
+    </div>
+  </div>
+@endsection
