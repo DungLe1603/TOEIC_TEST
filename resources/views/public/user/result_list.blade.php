3549bc75fa1d232b4e895cbdd646cab5d62ae288
unknown <dungle160396@gmail.com>
2019-06-12 15:57:52 +0700

    [profile] update test result list with no data

--- resources/views/public/user/result_list.blade.php
+++ resources/views/public/user/result_list.blade.php
@@ -7,26 +7,30 @@
 				<h3 class="box-title">@lang('result.list')</h3>
 			</div>
 			<div class="box-body">
-				<table class="table table-bordered">
-					<tr>
-						<th style="width: 10px">@lang('common.table.num')</th>
-						<th>@lang('result.table.test')</th>
-						<th>@lang('result.table.listening_score')</th>
-						<th>@lang('result.table.reading_score')</th>
-						<th>@lang('result.table.total_score')</th>
-						<th>@lang('result.table.test_date')</th>
-					</tr>
-					@foreach ($results as $key => $result)
+				@if (count($results) == 0)
+					<h4>You have not taken any tests!</h4>	
+				@else
+					<table class="table table-bordered">
 						<tr>
-							<td>{{ $key + 1 }}</td>
-							<td>{{ $result->test->name }}</td>
-							<td>{{ $result->listening_score }}</td>
-							<td>{{ $result->reading_score }}</td>
-							<td>{{ $result->listening_score + $result->reading_score }}</td>
-							<td>{{ date('d-m-Y', strtotime($result->created_at)) }}</td>
+							<th style="width: 10px">@lang('common.table.num')</th>
+							<th>@lang('result.table.test')</th>
+							<th>@lang('result.table.listening_score')</th>
+							<th>@lang('result.table.reading_score')</th>
+							<th>@lang('result.table.total_score')</th>
+							<th>@lang('result.table.test_date')</th>
 						</tr>
-					@endforeach
-				</table>
+						@foreach ($results as $key => $result)
+							<tr>
+								<td>{{ $key + 1 }}</td>
+								<td>{{ $result->test->name }}</td>
+								<td>{{ $result->listening_score }}</td>
+								<td>{{ $result->reading_score }}</td>
+								<td>{{ $result->listening_score + $result->reading_score }}</td>
+								<td>{{ date('d-m-Y', strtotime($result->created_at)) }}</td>
+							</tr>
+						@endforeach
+					</table>
+				@endif
 			</div>
 			<div class="box-footer clearfix">
 			</div>
